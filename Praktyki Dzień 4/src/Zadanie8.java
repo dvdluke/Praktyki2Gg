6b399fb4b7a9169fce592f4ab9e5111738ffad77
Łukasz Berliński <47150002+dvdluke@users.noreply.github.com>
2021-02-28 20:20:04 +0100

    Update Zadanie8.java

--- Praktyki Dzień 4/src/Zadanie8.java
+++ Praktyki Dzień 4/src/Zadanie8.java
@@ -7,20 +7,31 @@ public class Zadanie8
         Scanner postac = new Scanner(System.in);
         String nazwa;
         String rasa;
+        String pochodzenie;
+        String płeć;
         String klasa;
         String bron;
             System.out.println("Wybierz imię postaci:");
             nazwa = postac.nextLine();
             System.out.println("Wybierz rasę postaci:");
             rasa = postac.nextLine();
+            System.out.println("Wpisz miasto pochodzenia postaci:");
+            pochodzenie = postac.nextLine();
+            System.out.println("Wpisz płeć postaci:");
+            płeć = postac.nextLine();
             System.out.println("Wybierz klasę postaci:");
             klasa = postac.nextLine();
             System.out.println("Wybierz główną broń postaci:");
             bron = postac.nextLine();
             System.out.println("Stworzona przez ciebie postać to:");
-            System.out.println("Nazwa postaci: " + nazwa);
-            System.out.println("Rasa postaci: " + rasa);
-            System.out.println("Klasa postaci: " + klasa);
-            System.out.println("Broń postaci: " + bron);
+            System.out.println("Nazwa: " + nazwa);
+            System.out.println("Rasa: " + rasa);
+            System.out.println("Pochodzenie: " + pochodzenie);
+            System.out.println("Płeć: " + płeć);
+            System.out.println("Klasa: " + klasa);
+            System.out.println("Broń: " + bron);
+
+
+
         }
     }
